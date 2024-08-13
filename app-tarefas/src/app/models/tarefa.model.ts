36c8ca785308a0dcaf2cb3e75ac4222ba7d38808
PedroGuerraMP <pedrowar.pw@gmail.com>
2024-08-13 00:34:33 -0300

    add tarefa model

---
+++ app-tarefas/src/app/models/tarefa.model.ts
@@ -0,0 +1,13 @@
+export class Tarefa {
+    id?: number;
+    titulo?: string;
+    descricao?: string;
+    data_vencimento?: any;
+    status?: statusTarefa; 
+}
+
+enum statusTarefa {
+    pendente = 0,
+    em_andamento = 1,
+    concluida = 2
+}
