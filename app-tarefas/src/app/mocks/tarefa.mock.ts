722beffffaa6759d64176bd1946474068516581d
PedroGuerraMP <pedrowar.pw@gmail.com>
2024-08-13 03:41:19 -0300

    add getAll mock

---
+++ app-tarefas/src/app/mocks/tarefa.mock.ts
@@ -0,0 +1,25 @@
+import { Tarefa } from "../models/tarefa.model";
+
+export const mock_tarefa_list: Tarefa[] = [
+    {
+        id: 0, 
+        status: 0, 
+        data_vencimento: '13/08/2024 00:00:0000', 
+        descricao:'Tarefa Modelo 1', 
+        titulo: 'Titulo Modelo 1'
+    },
+    {
+        id: 1, 
+        status: 1, 
+        data_vencimento: '14/08/2024 00:00:0000', 
+        descricao:'Tarefa Modelo 2', 
+        titulo: 'Titulo Modelo 2'
+    },
+    {
+        id: 2, 
+        status: 2, 
+        data_vencimento: '15/08/2024 00:00:0000', 
+        descricao:'Tarefa Modelo 3', 
+        titulo: 'Titulo Modelo 3'
+    },
+]
--- app-tarefas/src/app/services/tarefa.service.ts
+++ app-tarefas/src/app/services/tarefa.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { Tarefa } from '../models/tarefa.model';
+import { mock_tarefa_list } from '../mocks/tarefa.mock';
 
 const baseUrl = 'http://localhost:3000/tarefas';
 
@@ -13,7 +15,10 @@ export class TarefaService {
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Tarefa[]> {
-    return this.http.get<Tarefa[]>(baseUrl);
+    // return this.http.get<Tarefa[]>(baseUrl);
+    return new Observable<Tarefa[]>(observer => {
+      setInterval(() => observer.next(mock_tarefa_list), 100);
+    });
   }
 
   get(id: number): Observable<Tarefa> {
