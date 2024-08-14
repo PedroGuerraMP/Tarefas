eba577ce301a7249ac8ccccf04596f9b6ee9a07b
PedroGuerraMP <pedrowar.pw@gmail.com>
2024-08-14 01:42:18 -0300

    add filtro por colunas

--- app-tarefas/src/app/components/list-tarefa/tarefa-list.component.ts
+++ app-tarefas/src/app/components/list-tarefa/tarefa-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 import { Tarefa } from '../../models/tarefa.model';
@@ -11,14 +13,21 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-tarefa-list',
   standalone: true,
-  imports: [CommonModule, MatTableModule,MatSlideToggleModule, MatExpansionModule ],
+  imports: [CommonModule, MatTableModule,MatSlideToggleModule, MatExpansionModule, MatButtonToggleModule, FormsModule ],
   templateUrl: './tarefa-list.component.html',
   styleUrl: './tarefa-list.component.css'
 })
 export class TarefaListComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'titulo', 'descricao'];
-  dataSource: Tarefa[] = [];
+  
   readonly panelOpenState = signal(false);
+  selectedFilter?: string = "";
+  tarefaDataSource: Tarefa[] = [];
+  filtrosDataSource: Map<string, string> = new Map([
+    [ "id", "Id" ],
+    [ "titulo", "Título" ],
+    [ "data_vencimento", "Data de Vencimento" ],
+    [ "status", "Status" ],
+  ]);
   
   constructor(private tarefaService: TarefaService) {}
 
@@ -29,8 +38,45 @@ export class TarefaListComponent implements OnInit {
   getTarefas(): void {
     this.tarefaService.getAll().subscribe(tarefas => { 
       console.log(tarefas[0].id);
-      this.dataSource = tarefas;
+      this.tarefaDataSource = tarefas;
     });
   }
 
+  filtroTarefaChange(tarefaProp: string): void {
+    this.tarefaDataSource.sort((a:Tarefa, b:Tarefa) => {
+      switch (tarefaProp) {
+        case "id":
+          if( a.id == null || b.id == null)
+            return 0;
+          else          
+          return a.id < b.id? -1 : 1;
+        break;
+
+        case "titulo":
+          if( a.titulo == null || b.titulo == null)
+            return 0;
+          else          
+          return a.titulo < b.titulo? -1 : 1;
+        break;
+
+        case "data_vencimento":
+          if( a.data_vencimento == null || b.data_vencimento == null)
+            return 0;
+          else          
+          return a.data_vencimento < b.data_vencimento? -1 : 1;
+        break;
+
+        case "status":
+          if( a.status == null || b.status == null)
+            return 0;
+          else          
+          return a.status <= b.status? -1 : 1;
+        break;
+      
+        default:
+          return 0;
+          break;
+      }
+    })
+  }
 }
--- app-tarefas/src/app/mocks/tarefa.mock.ts
+++ app-tarefas/src/app/mocks/tarefa.mock.ts
@@ -3,7 +3,7 @@ import { Tarefa } from "../models/tarefa.model";
 export const mock_tarefa_list: Tarefa[] = [
     {
         id: 0, 
-        status: 0, 
+        status: 2, 
         data_vencimento: '13/08/2024 00:00:0000', 
         descricao:'Tarefa Modelo 1', 
         titulo: 'Titulo Modelo 1'
@@ -17,7 +17,7 @@ export const mock_tarefa_list: Tarefa[] = [
     },
     {
         id: 2, 
-        status: 2, 
+        status: 0, 
         data_vencimento: '15/08/2024 00:00:0000', 
         descricao:'Tarefa Modelo 3', 
         titulo: 'Titulo Modelo 3'
