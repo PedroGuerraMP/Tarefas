79c64e601712c7bed364e5b36d3f0d66d28d7aff
PedroGuerraMP <pedrowar.pw@gmail.com>
2024-08-13 23:11:10 -0300

    rename path

--- app-tarefas/src/app/app.routes.ts
+++ app-tarefas/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { TarefaListComponent } from './components/tarefa-list/tarefa-list.component';
+import { TarefaListComponent } from './components/list-tarefa/tarefa-list.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'list', pathMatch: 'full' },
